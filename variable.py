78dd271bf0c54b29dca3977f6649f3fd5e1a5c2c
gracekelly626 <90355504+gracekelly626@users.noreply.github.com>
2021-09-11 12:47:06 +0100

    Update variable.py

--- variable.py
+++ variable.py
@@ -1,3 +1,12 @@
 # variable (like a container)
 # number: int, float, double - 双精度float，占双倍内存。 （内存给CPU计算的， 硬盘是用来长期储存文件） 
 # string: "xx", (python doesn't diffentiate "" vs '')
+
+print('Hello, world!')
+print(1+1)
+
+number1 = 1
+number2 = 2
+number3 = 3.3
+mysum = number1+number2+number3
+print(mysum)
