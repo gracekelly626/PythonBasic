6f406c09cafc366f46842e9b828971c9aa0f8555
gracekelly626 <90355504+gracekelly626@users.noreply.github.com>
2021-09-11 12:51:25 +0100

    Update variable.py

--- variable.py
+++ variable.py
@@ -10,3 +10,8 @@ number2 = 2
 number3 = 3.3
 mysum = number1+number2+number3
 print(mysum)
+
+radius = 3.3333
+pia = 3.14
+area = pia*radius*radius ## for almost every programming language; for pyhton specific - area = pia*radius**2
+print(area)
