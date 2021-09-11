7d8503e2ba54e2b3be132f5de37ece32d6539bde
gracekelly626 <90355504+gracekelly626@users.noreply.github.com>
2021-09-11 13:06:46 +0100

    Update if_statement.py

--- if_statement.py
+++ if_statement.py
@@ -1,1 +1,9 @@
-
+area = 0
+if area <= 0:
+  print('this is not valid:', area)
+elif area >= 50:
+  print('this is such a big circle:', area)
+elif 50> area >= 40:
+  print('this is such a medium circle:', area)
+else:
+  print('this is such a small circle:', area)
