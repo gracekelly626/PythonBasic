d783261167b1bae898c1bac12a8018e2384911e7
gracekelly626 <90355504+gracekelly626@users.noreply.github.com>
2021-09-18 15:30:46 +0100

    Update 2. for&while_loop.py

--- 2. for&while_loop.py
+++ 2. for&while_loop.py
@@ -1,4 +1,26 @@
-sum_100 = 0
-for i in range(1,101):
-  sum_100 = sum_100 + i
-print(sum_100)
+## for loop
+def sum_any(x):
+  result = 0
+  for i in range(1,x+1,2):
+    result = result+i
+    print(i)
+  return result
+print(sum_any(10))
+
+
+## while loop
+result = 0
+while result < 100: #statement is usually boolean
+  result = result + 1
+
+print(result)
+
+
+## while + break
+result = 0
+while result < 100:
+  result = result + 1
+  if result == 25:
+    break #break the running outside while loop 
+
+print(result)
