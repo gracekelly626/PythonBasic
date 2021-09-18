7654022f4bf81101e63ee2779a53506832ced9d9
gracekelly626 <90355504+gracekelly626@users.noreply.github.com>
2021-09-18 15:48:22 +0100

    Update 2. for&while_loop.py

--- 2. for&while_loop.py
+++ 2. for&while_loop.py
@@ -12,8 +12,20 @@ print(sum_any(10))
 result = 0
 while result < 100: #statement is usually boolean
   result = result + 1
-
 print(result)
+
+
+## while loop in a function
+def sum_any(x):
+  result = 0
+  i = 0
+  while i < x:
+    i = i+1
+    result = result+i
+    print(i)
+  return result
+print(sum_any(10))
+
 
 
 ## while + break
@@ -22,5 +34,16 @@ while result < 100:
   result = result + 1
   if result == 25:
     break #break the running outside while loop 
-
 print(result)
+
+
+## while vs if --- WHILE LOOP keeps excuting until it reaches to the boolen statement; IF only runs once and stops. 
+def sum_any(x):
+  result = 0
+  i = 0
+  if i < x:
+    i = i+1
+    result = result+i
+    print(i)
+  return result
+print(sum_any(10))
