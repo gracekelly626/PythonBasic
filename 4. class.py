97f1f215f36b457bdb1bc7943dd394e10bdb6c8b
gracekelly626 <90355504+gracekelly626@users.noreply.github.com>
2021-09-18 16:10:15 +0100

    Update 4. class.py

--- 4. class.py
+++ 4. class.py
@@ -1,1 +1,18 @@
 
+## class is like the blueprint. can tell people how to create instances
+## instance aka object
+class Person:
+##__init__ a special method of class in Python - constructor is a general programming term for creating an instance of a class
+  def __init__(self, name, age, address):
+## self is usually the first parameter of a method
+    self.m_name = name
+    self.m_age = age
+    self.m_address = address
+
+  def getName(self):
+## even the getName method doesn't have any specific parameters, should put 'self' as the first parameter. 
+      return self.m_name
+
+k = Person("Kuangdi", 3, "E14")
+print(k.getName())
+#function of a class aka method 
